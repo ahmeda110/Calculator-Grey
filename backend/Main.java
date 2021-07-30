deede7fb4627c0a5273f63727039727e620c396d
Ahmed-Abbas <65793945+ahmeda110@users.noreply.github.com>
2021-07-30 00:09:33 -0600

    Update Main.java

--- backend/Main.java
+++ backend/Main.java
@@ -5,15 +5,14 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(8080);
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                new RequestProcessor(clientSocket).run();
-            }
-        } catch (IOException e) {
-        }
-    }
+  public static void main(String[] args) {
+    ServerSocket serverSocket = null;
+    try {
+      serverSocket = new ServerSocket(8080);
+      while (true) {
+        Socket clientSocket = serverSocket.accept();
+        new RequestProcessor(clientSocket).run();
+      }
+    } catch (IOException e) {}
+  }
 }
